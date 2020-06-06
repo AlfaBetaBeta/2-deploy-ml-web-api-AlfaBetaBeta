115bca43bc410c7a7bd411336f5080bf9c822202
Alejandro Barrero <alejandro.barrero.bilbao@gmail.com>
2020-06-06 13:24:17 +0200

    Added handling of url parameters

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, request, abort
 
 import sklearn
 import ie_bike_model
-from ie_bike_model.model import train_and_persist
+from ie_bike_model.model import train_and_persist, predict
 
 import argparse
 from platform import python_version
+import datetime as dt
 
 app = Flask(__name__)
 
@@ -19,6 +20,7 @@ def api_get_versions():
     "Python": python_version(),
     }
 
+
 @app.route("/train_and_persist")
 def api_train_and_persist():
     """Endpoint to train and persist the model defined in lib/"""
@@ -28,6 +30,23 @@ def api_train_and_persist():
     }
 
 
+@app.route("/predict")
+def api_predict():
+    """Endpoint to retrieve model predictions from URL parameters"""
+    args = dict(request.args)
+    
+    try:
+    	args["date"] = dt.datetime.strptime(args["date"], "%Y-%m-%dT%H:%M:%S")
+    	args["weathersit"] = int(args["weathersit"])
+    	args["temperature_C"] = float(args["temperature_C"])
+    	args["feeling_temperature_C"] = float(args["feeling_temperature_C"])
+    	args["humidity"] = float(args["humidity"])
+    	args["windspeed"] = float(args["windspeed"])
+
+    except:
+        abort(400)
+
+    return {"result": predict(args)}
 
 
 if __name__ == '__main__':
