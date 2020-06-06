922ab29f07e575dc6769bac3a07e09c3592daf15
Alejandro Barrero <alejandro.barrero.bilbao@gmail.com>
2020-06-06 23:34:31 +0200

    Applied black format

--- app.py
+++ app.py
@@ -11,16 +11,16 @@ import time
 
 
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
 
 
 @app.route("/")
 def api_get_versions():
     """Basic endpoint displaying relevant versions"""
     return {
-    "scikit-learn": sklearn.__version__,
-    "ie-bike-model": ie_bike_model.__version__,
-    "Python": python_version(),
+        "scikit-learn": sklearn.__version__,
+        "ie-bike-model": ie_bike_model.__version__,
+        "Python": python_version(),
     }
 
 
@@ -28,7 +28,7 @@ def api_get_versions():
 def api_train_and_persist():
     """Endpoint to train and persist the model defined in lib/"""
     regressor = train_and_persist(compression_factor=True)
-    
+
     return {"status": "ok", "persisted-model-parameters": regressor.get_params()}
 
 
@@ -37,23 +37,28 @@ def api_predict():
     """Endpoint to retrieve model predictions from URL parameters"""
     start_time = time.time()
     args = dict(request.args)
-    
+
     try:
-    	args["date"] = dt.datetime.strptime(args["date"], "%Y-%m-%dT%H:%M:%S")
-    	args["weathersit"] = int(args["weathersit"])
-    	args["temperature_C"] = float(args["temperature_C"])
-    	args["feeling_temperature_C"] = float(args["feeling_temperature_C"])
-    	args["humidity"] = float(args["humidity"])
-    	args["windspeed"] = float(args["windspeed"])
+        args["date"] = dt.datetime.strptime(args["date"], "%Y-%m-%dT%H:%M:%S")
+        args["weathersit"] = int(args["weathersit"])
+        args["temperature_C"] = float(args["temperature_C"])
+        args["feeling_temperature_C"] = float(args["feeling_temperature_C"])
+        args["humidity"] = float(args["humidity"])
+        args["windspeed"] = float(args["windspeed"])
 
     except:
-        abort(400, description="400: Missing/incorrect URL parameters.\n\nMake sure these are passed as:\n\n?date\n&weathersit\n&temperature_C\n&feeling_temperature_C\n&humidity\n&windspeed")
+        abort(
+            400,
+            description="400: Missing/incorrect URL parameters.\n\nMake sure these are passed as:\n\n?date\n&weathersit\n&temperature_C\n&feeling_temperature_C\n&humidity\n&windspeed",
+        )
 
     return {"result": predict(args), "elapsed_time": round(time.time() - start_time, 3)}
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Flask application to deploy ie-bike-model")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Flask application to deploy ie-bike-model"
+    )
     parser.add_argument("-d", "--debug", action="store_true", help="Enable debug mode")
     args = parser.parse_args()
     app.run(debug=args.debug)
