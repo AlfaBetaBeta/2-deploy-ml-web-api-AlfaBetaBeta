8ed17c7b5f395f0c7289df4b5b5287563fc89a92
Alejandro Barrero <alejandro.barrero.bilbao@gmail.com>
2020-06-06 11:27:37 +0200

    Modified app to retrieve library versions

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask,request,abort
-import argparse
+from flask import Flask, request, abort
+
 import sklearn
-from ie_nlp_utils.tokenization import tokenize as tk
+# from ie_nlp_utils.tokenization import tokenize as tk
+
+import argparse
+from platform import python_version
 
 app = Flask(__name__)
 
+@app.route("/")
+def api_get_versions():
+    """Sample endpoints with dynamic URLs"""
+    # args = dict(request.args)
+    return {
+    "scikit-learn": sklearn.__version__,
+    "Python": python_version(),
+    }
