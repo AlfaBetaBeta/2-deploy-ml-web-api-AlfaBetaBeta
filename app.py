abe57731d1e35922268513f6ac3d36cb1062b0f7
Alejandro Barrero <alejandro.barrero.bilbao@gmail.com>
2020-06-06 11:47:10 +0200

    Enabled app.run with debug opt arg

--- app.py
+++ app.py
@@ -11,10 +11,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def api_get_versions():
-    """Sample endpoints with dynamic URLs"""
-    # args = dict(request.args)
+    """Basic endpoint displaying relevant versions"""
     return {
     "scikit-learn": sklearn.__version__,
     "ie-bike-model": ie_bike_model.__version__,
     "Python": python_version(),
     }
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Flask application to deploy ie-bike-model")
+    parser.add_argument("-d", "--debug", action="store_true", help="Enable debug mode")
+    args = parser.parse_args()
+    app.run(debug=args.debug)
